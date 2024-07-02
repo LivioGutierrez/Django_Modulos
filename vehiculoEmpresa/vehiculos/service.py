26e4c3c951267923d289474dc9041c8200004a4b
Livio Gutierrez <livio.gutierrez.v@gmail.com>
2024-07-01 21:09:50 -0400

    subiendo avance del desafio-vehiculo

--- vehiculoEmpresa/vehiculos/service.py
+++ vehiculoEmpresa/vehiculos/service.py
@@ -9,25 +9,51 @@ def crear_chofer(pRut, pNombre, pApellido, pActivo, pCreacion_registro):
     chofer.save()
 
 def crear_registro_contable(pFecha_compra, pValor):
-    registro_contable = registro_contable(fecha_compra = pFecha_compra, pValor = pValor)
+    registro_contable = RegistroContabilidad(fecha_compra = pFecha_compra, pValor = pValor)
     registro_contable.save()
 
-def deshabilitar_chofer():
-    pass
+def deshabilitar_chofer(rut_chofer, activo = False):
+    obj_chofer = Chofer.objects.get(pk= rut_chofer)
+    
+    deshabilitarChofer = Chofer(obj_chofer, activo = activo)
+    deshabilitarChofer.save()
 
-def deshabilitar_vehiculo():
-    pass
+def deshabilitar_vehiculo(patante_vehiculo, activo = False):
+    obj_vehiculo = Vehiculo.objects.get(pk = patante_vehiculo)
+    
+    deshabilitarVehiculo = Vehiculo(obj_vehiculo, activo = activo)
+    deshabilitarVehiculo.save()
 
-def habilitar_chofer():
-    pass
+def habilitar_chofer(rut_chofer, activo = True):
+    obj_chofer = Chofer.objects.get(pk= rut_chofer)
+    
+    deshabilitarChofer = Chofer(obj_chofer, activo = activo)
+    deshabilitarChofer.save()
 
-def habilitar_vehiculo():
-    pass
+def habilitar_vehiculo(patante_vehiculo, activo = True):
+    obj_vehiculo = Vehiculo.objects.get(pk = patante_vehiculo)
+    
+    deshabilitarVehiculo = Vehiculo(obj_vehiculo, activo = activo)
+    deshabilitarVehiculo.save()
 
 def obtener_vehiculo():
-    pass
+    lista_vehiculo = Vehiculo.objects.all()# Cambiar, creo que aca solo va esta linea y en la funcion " imprimir_datos_vehiculos" va el for
+    for lista in lista_vehiculo:
+        print(f"Patente: {lista.patente}\n 
+                Marca: {lista.marca}\n 
+                Modelo: {lista.modelo}\n 
+                Año: {lista.year}\n 
+                Activo: { lista.activo}")
 
 def obtener_chofer():
+    lista_chofer = Chofer.objects.all()
+    for lista in lista_chofer:
+        print(f"Rut: {lista.rut}\n 
+            Nombre: {lista.nombre}\n 
+            Apellido: {lista.apellido}\n 
+            Fecha de registro: {lista.creacion_registro}\n 
+            Vehiculo: {lista.vehiculo}\n 
+            Activo: {lista.activo}")
     pass
 
 def asignar_chofer_a_vehiculo():
