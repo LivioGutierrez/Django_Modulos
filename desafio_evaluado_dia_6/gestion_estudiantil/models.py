9b67f360cea68ed4807c081f0bc755b5585e3482
Livio Gutierrez <livio.gutierrez.v@gmail.com>
2024-07-02 16:27:22 -0400

    terminando el model

--- desafio_evaluado_dia_6/gestion_estudiantil/models.py
+++ desafio_evaluado_dia_6/gestion_estudiantil/models.py
@@ -1,31 +1,40 @@
 from django.db import models
 
+class Curso(models.Model):
+    codigo = models.CharField(max_length=10, null=False, unique=True, primary_key=True)
+    nombre = models.CharField(max_length = 50, null = False)
+    version = models.IntegerField()
+    
 class Profesor(models.Model):
     rut = models.CharField(max_length = 9, null = False, primary_key=True)
-    nombre = models.CharField(max_length = 50, null = False)
-    apellido = models.CharField(max_length = 50, null = False)
+    nombre = models.CharField(max_length = 50, null = False, blank=False)
+    apellido = models.CharField(max_length = 50, null = False, blank=False)
     activo = models.BooleanField(default=False)
     creacion_registro = models.DateTimeField()
     modificacion_registro = models.DateTimeField()
     creado_por = models.CharField(max_length = 50, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class Curso(models.Model):
-    codigo = models.CharField(max_length=10, null=False, unique=True)
-    nombre = models.CharField(max_length = 50, null = False)
-    version = models.IntegerField()
-    # profesor_id = models.CharField(max_length=9, unique=True) creo que con esto estaria relacionada
-
+    cursos = models.ManyToManyField(Curso, related_name='Profesores')
+    
 class Estudiante(models.Model):
-    rut = models.CharField(max_length= 9, null=False)
-    nombre = models.CharField(max_length = 50, null = False)
-    apellido = models.CharField(max_length = 50, null = False)
-    fecha_nac = models.DateTimeField(null=False)
-    activo = models.BooleanField(default=False)
+    rut = models.CharField(max_length= 9, null=False, blank=False, primary_key=True)
+    nombre = models.CharField(max_length = 50, null = False, blank=False)
+    apellido = models.CharField(max_length = 50, null = False, blank=False)
+    fecha_nac = models.DateTimeField(null=False, blank=False)
+    activo = models.BooleanField(default=False, blank=False)
     creacion_registro = models.DateTimeField()
     modificacion_registro = models.DateTimeField()
     creado_por = models.CharField(max_length=50, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    curso = models.ForeignKey(Curso, null= False, blank= False,related_name='estudiantes', on_delete=models.CASCADE )
 
 class Direccion(models.Model):
-    pass
+    calle = models.CharField(max_length = 50, null=False) 
+    numero = models.IntegerField(max_length=10, null=False)
+    dpto = models.CharField(max_length=10, default="")
+    comuna = models.CharField(max_length=50, null=False)
+    ciudad = models.CharField(max_length=50, null= False)
+    region = models.CharField(max_length=50, null= False)
+    estudiante = models.OneToOneField(Estudiante, related_name='direcciones', null= False, on_delete=models.CASCADE)
